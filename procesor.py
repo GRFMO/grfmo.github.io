4b8c1be18fdbd2bf19d19ee214d7c9fd941ea1b8
Roosevelt Ubaldo <roosevelt.ubaldo@outlook.com>
2023-09-09 00:47:47 -0500

    --amend

--- procesor.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        print(f"- name: {row[1]}")
-        print(f"  degree: {row[4]}")
-        print(f"  email: {row[2]}")
